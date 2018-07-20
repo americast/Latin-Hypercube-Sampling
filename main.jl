ac5bb9325fe9d67c32dc7f0c9c4220e72e0dfcd2
americast <sayan.sinha@iitkgp.ac.in>
2018-07-20 17:21:17 +0530

    Add 2D

--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
-f_inv(x) = sin(x)
+f_inv(x) = abs(sin(x))
 n = 10
+
 
 function latin_hypercube_sampling_1d(f_inv, n)
 	l_limit = 0.0 : (n-1)
@@ -7,5 +8,13 @@ function latin_hypercube_sampling_1d(f_inv, n)
 	f_inv.(rand_nos)
 end
 
+function latin_hypercube_sampling_2d(f_inv, n)
+	l_limit = 0.0 : (n-1)
+	value_1 = l_limit + latin_hypercube_sampling_1d(f_inv, n)
+	value_2 = l_limit + latin_hypercube_sampling_1d(f_inv, n)
+	value_1 = value_1[randperm(n)]
+	value_2 = value_2[randperm(n)]
+	value = hcat(value_1, value_2)
+end
 
-println(latin_hypercube_sampling_1d(f_inv, n))
+println(latin_hypercube_sampling_2d(f_inv, n))
