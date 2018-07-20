8644963848cb8410db1624297ee97509920d17b5
americast <sayan.sinha@iitkgp.ac.in>
2018-07-20 16:33:10 +0530

    Change function to sin

--- main.jl
+++ main.jl
@@ -1,7 +1,11 @@
-f_inv(x) = log(x)
+f_inv(x) = sin(x)
+n = 10
 
 function latin_hypercube_sampling_1d(f_inv, n)
 	l_limit = 0.0 : (n-1)
 	rand_nos = rand(n) + l_limit
 	f_inv.(rand_nos)
 end
+
+
+println(latin_hypercube_sampling_1d(f_inv, n))
