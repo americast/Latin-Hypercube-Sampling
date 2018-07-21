e6a0588a4d6e1edd1fb82c173189e936b2a46b3d
americast <sayan.sinha@iitkgp.ac.in>
2018-07-21 20:18:46 +0530

    Support multiple dims

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 f_inv(x) = abs(sin(x))
 n = 10
-
+dims = 3
 
 function latin_hypercube_sampling_1d(f_inv, n)
 	l_limit = 0.0 : (n-1)
@@ -8,13 +8,13 @@ function latin_hypercube_sampling_1d(f_inv, n)
 	f_inv.(rand_nos)
 end
 
-function latin_hypercube_sampling_2d(f_inv, n)
+function latin_hypercube_sampling_nd(f_inv, n, dims)
 	l_limit = 0.0 : (n-1)
-	value_1 = l_limit + latin_hypercube_sampling_1d(f_inv, n)
-	value_2 = l_limit + latin_hypercube_sampling_1d(f_inv, n)
-	value_1 = value_1[randperm(n)]
-	value_2 = value_2[randperm(n)]
-	value = hcat(value_1, value_2)
+	value = []
+	for i in 1:dims
+		push!(value, l_limit + latin_hypercube_sampling_1d(f_inv, n))
+	end
+	hcat(value...)
 end
 
-println(latin_hypercube_sampling_2d(f_inv, n))
+println(latin_hypercube_sampling_nd(f_inv, n, dims))
